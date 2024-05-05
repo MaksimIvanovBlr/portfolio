650ace8012f38d899696a4a40338b38c452d3d57
Maksim Ivanov <ivanov.maksim.blr@gmail.com>
2024-05-05 12:26:53 +0300

    change the status when clicking on the button

--- src/components/btnDarkMode/BtnDarkMode.js
+++ src/components/btnDarkMode/BtnDarkMode.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, {useEffect, useState, useRef} from 'react';
 import "./style.css"
 import sun from "./sun.svg";
 import moon from "./moon.svg";
 
 const BtnDarkMode = (props) => {
 
+    const [darkMode, setDarkMode] = useState('light')
+    const btnRef = useRef(null)
+
+    useEffect(()=>{
+        if (darkMode === 'dark'){
+            document.body.classList.add('dark')
+            btnRef.current.classList.add('dark-mode-btn--active')
+        }
+        else{
+            document.body.classList.remove('dark')
+            btnRef.current.classList.remove('dark-mode-btn--active')
+        }
+    },[darkMode])
+
+    const toogleNightMode = () => {
+        setDarkMode((currentValue) => {return currentValue === 'light'? 'dark': 'light'})
+    }
+
+
     return(
-        <button className="dark-mode-btn">
+
+        <button ref={btnRef} className="dark-mode-btn" onClick={toogleNightMode}>
             <img src={sun} alt="Light mode" className="dark-mode-btn__icon"/>
             <img src={moon} alt="Dark mode" className="dark-mode-btn__icon"/>
         </button>
