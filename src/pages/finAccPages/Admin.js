7d8e2161f7e8b255d85364eda8f39908309123c2
Maksim Ivanov <ivanov.maksim.blr@gmail.com>
2024-05-10 13:01:03 +0300

    basic layout of admin pannel

--- src/pages/finAccPages/Admin.js
+++ src/pages/finAccPages/Admin.js
@@ -3,7 +3,24 @@ import React from 'react';
 const Admin = () => {
     return (
         <div>
-            admin
+            <a href="#!" className="btnFin">бюджет</a>
+            <a href="#!" className="btnFin">дневной остаток</a>
+            <a href="#!" className="btnFin">user settings </a>
+            <ul>
+                <li className='list-acc'>
+                    пользователи
+                </li>
+                <li className='list-acc'>
+                    посетители сайта
+                </li>
+                <li className='list-acc'>
+                    заблокированные пользователи
+                </li>
+                <li className='list-acc'>
+                    закрыть регистарцию
+                </li>
+
+            </ul>
         </div>
     );
 };
