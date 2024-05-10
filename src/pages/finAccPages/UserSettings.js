50e6e2fbc56270a0e483dfc672af0dd702be5c9f
Maksim Ivanov <ivanov.maksim.blr@gmail.com>
2024-05-10 12:55:31 +0300

    basic layout of user-settings

--- src/pages/finAccPages/UserSettings.js
+++ src/pages/finAccPages/UserSettings.js
@@ -3,7 +3,29 @@ import React from 'react';
 const UserSettings = () => {
     return (
         <div>
-            user settings
+            <a href="#!" className="btnFin">бюджет</a>
+            <a href="#!" className="btnFin">дневной остаток</a>
+            <a href="#!" className="btnFin">user settings </a>
+            <ul>
+                <li className='list-acc'>
+                    Расходы
+                </li>
+                <li className='list-acc'>
+                    Основной доход
+                </li>
+                <li className='list-acc'>
+                    Дополнительный доход
+                </li>
+                <li className='list-acc'>
+                    Изменить резерв
+                </li>
+                <li className='list-acc'>
+                    Основные данные
+                </li>
+                <li className='list-acc'>
+                    Пересчитать резерв
+                </li>
+            </ul>
         </div>
     );
 };
